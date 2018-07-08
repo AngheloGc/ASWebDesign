e757d0220484f640c53c57a96d8b667400d07279
Anghelo <agiron_1907@hotmail.com>
2018-07-08 02:14:12 -0500

    Mobile Fixed
    Mobile :3

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+let mobileMenuButton = document.getElementById('mobile-menu-button');
+let mobileMenuBox = document.getElementById('mobile-menu-box');
+
+mobileMenuButton.addEventListener('click', function() {
+
+    mobileMenuBox.classList.toggle('appear');
+
+});
+    
